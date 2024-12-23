5c8a59a639ca8767ef64b3021774bae8263c47f5
Ajay Kumar Verma <ajayverma128@gmail.com>
2024-12-24 00:43:16 +0530

    Time: 1 ms (70.01%) | Memory: 52.7 MB (72.92%) - LeetSync

---
+++ 98-validate-binary-search-tree/validate-binary-search-tree.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isValidBST = function(root) {
+    function isValid(node, min, max) {
+        if (node === null) return true;
+
+        if (node.val < min || node.val > max) return false;
+
+        return isValid(node.left, min, node.val - 1) &&
+            isValid(node.right, node.val + 1, max);
+    }
+
+    return isValid(root, Number.MIN_SAFE_INTEGER, Number.MAX_SAFE_INTEGER);
+};
