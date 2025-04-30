34e7195ba3f41550e9e6f9826617385769971d69
Ajay Kumar Verma <ajayverma128@gmail.com>
2025-04-30 22:50:44 +0530

    Time: 3 ms (9.80%) | Memory: 55.5 MB (7.19%) - LeetSync

--- 1086-divisor-game/divisor-game.js
+++ 1086-divisor-game/divisor-game.js
@@ -6,7 +6,7 @@ var divisorGame = function(n) {
     let dp = new Array(n+1).fill(false);
 
     for (let i = 2; i <= n; i++) {
-        for (let j = 1; j < i; j++) {
+        for (let j = 1; j * j < i; j++) {
             if (i % j === 0 && !dp[i-j]) {
                 dp[i] = true;
             }
