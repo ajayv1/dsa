1b1dbdec54cfc63803a670f820179a20d1323d6b
Ajay Kumar Verma <ajayverma128@gmail.com>
2025-05-06 12:22:25 +0530

    Time: 1 ms (87.30%) | Memory: 63.2 MB (44.44%) - LeetSync

---
+++ 513-find-bottom-left-tree-value/find-bottom-left-tree-value.js
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+
+var levelOrderTraversal = function(root) {
+    if (!root) return [];
+
+    let queue = [root], ans = [];
+    let count = 0;
+
+    while (count < queue.length) {
+        let size = queue.length - count;
+        let curLevel = [];
+
+        for (let i = 0; i < size; i++) {
+            let cur = queue[count++];
+            curLevel.push(cur.val);
+
+            if (cur.left) queue.push(cur.left);
+            if (cur.right) queue.push(cur.right);
+        }
+
+        // add every level last child for right side view
+        ans.push(curLevel);
+    }
+
+    return ans;
+};
+
+var findBottomLeftValue = function(root) {
+    const levelOrder = levelOrderTraversal(root);
+
+    return levelOrder[levelOrder.length - 1][0];
+};
