07241f89a33033c859b4d168fd62c78553f7d710
Ajay Kumar Verma <ajayverma128@gmail.com>
2025-05-15 11:47:38 +0530

    Time: 8 ms (72.50%) | Memory: 66.4 MB (23.57%) - LeetSync

--- 56-merge-intervals/merge-intervals.js
+++ 56-merge-intervals/merge-intervals.js
@@ -4,19 +4,20 @@
  */
 var merge = function(intervals) {
     if (intervals?.length === 0) return [];
-    let sorted = intervals.slice().sort((a, b) => a[0] - b[0]);
-    let result = [sorted[0]];
+
+    const sorted = intervals.sort((a, b) => a[0] - b[0]);
+    let ans = [sorted[0]];
 
     for (let i = 1; i < sorted.length; i++) {
-        let [curS, curE] = sorted[i];
-        let last = result[result.length - 1];
+        let [s, e] = sorted[i];
+        let last = ans[ans.length - 1];
 
-        if (curS <= last[1]) {
-            last[1] = Math.max(curE, last[1]);
+        if (s <= last[1]) {
+            last[1] = Math.max(e, last[1]);
         } else {
-            result.push(sorted[i]);
+            ans.push(sorted[i]);
         }
     }
 
-    return result;
+    return ans;
 };
